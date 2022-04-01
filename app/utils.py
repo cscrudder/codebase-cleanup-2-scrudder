5008652d1ccbab8aaca4963bccc7a47adaf6f8d5
cscrudder <cms447@georgetown.edu>
2022-04-01 10:01:05 -0400

    Fixes errors with groceries

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,11 @@
 
 # This function formats float into USD
 def to_usd(price):
+    """
+    This function returns the formatted USD value of an input price.
+    For example, 1234.56 would become $1234.56.
+    The only input to the function is the number to be formatted.
+    This function can be used in many applications to make a readable output for the user.
+    """
     return '${:,.2f}'.format(price)
 
-print(to_usd(3234))
