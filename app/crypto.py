564c81935f30ebeb090cd5b13f41538e6581e342
cscrudder <97316514+cscrudder@users.noreply.github.com>
2022-04-01 17:58:54 -0400

    Financials (#7)
    * Fixes formatting redundancies of stocks and crypto
    
    * Completes the base requirements for the financials branch

--- app/crypto.py
+++ app/crypto.py
@@ -1,25 +1,12 @@
+from app.alphavantage_service import fetch_crypto_data
 
-
+symbol = input("Please input a crypto symbol (default: 'BTC'): ") or "BTC"
 
 print("CRYPTO REPORT...")
 
-import os
-import json
-from dotenv import load_dotenv
-import requests
+tsd = fetch_crypto_data(symbol)
 
-load_dotenv()
-
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
-
-symbol = input("Please input a crypto symbol (default: 'BTC'): ") or "BTC"
-url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&market=USD&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
-response = requests.get(url)
-parsed_response = json.loads(response.text)
-#print(parsed_response)
-#breakpoint()
-
-tsd = parsed_response["Time Series (Digital Currency Daily)"]
+from app.utils import to_usd
 
 dates = list(tsd.keys())
 latest_date = dates[0]
@@ -30,4 +17,4 @@ latest = tsd[latest_date]
 print(symbol)
 print(latest_date)
 print(latest['4a. close (USD)'])
-print('${:,.2f}'.format(float(latest['4a. close (USD)'])))
+print(to_usd(float(latest['4a. close (USD)'])))
